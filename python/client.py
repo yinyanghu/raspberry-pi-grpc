44be52c0301260ddfea57bcb8b494396bffe2cc0
Jian Li <lijianxp2005@gmail.com>
2020-09-19 23:38:31 -0400

    add parameterized server address and log messages

--- python/client.py
+++ python/client.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 import grpc
 
@@ -7,14 +8,16 @@ import adafruit_pb2_grpc
 import common
 from google.protobuf import empty_pb2
 
-def run():
-    with grpc.insecure_channel('localhost:{}'.format(common.PORT)) as channel:
+
+def run(server):
+    logging.info("connecting to server {}".format(server))
+    with grpc.insecure_channel('{}:{}'.format(server, common.PORT)) as channel:
         stub = adafruit_pb2_grpc.AHT20Stub(channel)
         response = stub.Measure(request=empty_pb2.Empty())
-    print('Temperature: {}'.format(response.temperature))
-    print('Relative Humidity: {}'.format(response.temperature))
+    print('Temperature: {:.2f}°C'.format(response.temperature))
+    print('Relative Humidity: {:.2f}%'.format(response.relative_humidity))
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
-    run()
+    logging.basicConfig(level=logging.WARNING)
+    run(sys.argv[1])
